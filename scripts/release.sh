7e322eab12b510329c64342edbfdf0abb38cabde
Laura Fitzgerald <lfitzger@redhat.com>
2020-05-12 10:23:06 +0100

    Reset upgrade variables after final release release-9.30.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -70,59 +70,59 @@ else
     fi
 fi
 
-echo "cutting ${RELEASE_TYPE} release ${releaseTag} from branch ${REMOTE}/${baseBranch}"
+# echo "cutting ${RELEASE_TYPE} release ${releaseTag} from branch ${REMOTE}/${baseBranch}"
 
-#do a fetch
-git fetch ${REMOTE}
+# #do a fetch
+# git fetch ${REMOTE}
 
-#check we have no local changes
-dirty=$(git ls-files -m | wc -l)
-if [[ "${dirty}" -gt "0" ]]
-then
-echo "
- the local file system is dirty cannot continue
-"
-exit 1
-fi
+# #check we have no local changes
+# dirty=$(git ls-files -m | wc -l)
+# if [[ "${dirty}" -gt "0" ]]
+# then
+# echo "
+#  the local file system is dirty cannot continue
+# "
+# exit 1
+# fi
 
-trap reset_current_branch EXIT
+# trap reset_current_branch EXIT
 
-# check if the specified from branch already exists if it does check it out otherwise create it
-git checkout -B ${baseBranch} ${REMOTE}/${baseBranch}
-if [[ $? > 0 ]]
-then
-echo "branch ${baseBranch} does not exist or you have local changes. Please create it and push it to ${REMOTE} before running the release"
-exit 1
-fi
+# # check if the specified from branch already exists if it does check it out otherwise create it
+# git checkout -B ${baseBranch} ${REMOTE}/${baseBranch}
+# if [[ $? > 0 ]]
+# then
+# echo "branch ${baseBranch} does not exist or you have local changes. Please create it and push it to ${REMOTE} before running the release"
+# exit 1
+# fi
 
-git reset --hard HEAD
+# git reset --hard HEAD
 
-releaseExists=$(git tag | grep ${^releaseTag$} | wc -l)
-if [[ "${releaseExists}" -gt "0" ]]
-then
-echo "
-a release with that name already exists
-"
-exit 1
-fi
+# releaseExists=$(git tag | grep ${^releaseTag$} | wc -l)
+# if [[ "${releaseExists}" -gt "0" ]]
+# then
+# echo "
+# a release with that name already exists
+# "
+# exit 1
+# fi
 
-sed -i.bak -E "s/^integreatly_version: .*$/integreatly_version: ${releaseTag}/g"  ./inventories/group_vars/all/manifest.yaml && rm ./inventories/group_vars/all/manifest.yaml.bak
+# sed -i.bak -E "s/^integreatly_version: .*$/integreatly_version: ${releaseTag}/g"  ./inventories/group_vars/all/manifest.yaml && rm ./inventories/group_vars/all/manifest.yaml.bak
 
-#commit the change, tag
-git commit -am "release manifest version update for ${releaseTag}"
-git tag ${releaseTag}
+# #commit the change, tag
+# git commit -am "release manifest version update for ${releaseTag}"
+# git tag ${releaseTag}
 
-#reset upgrade playbook and variables if this is the final release
-if [[ -z $LABEL_VERSION ]]; then
-    echo "resetting upgrade playbook and variables after final release $releaseTag"
-    cp scripts/upgrade.template.yml playbooks/upgrade.yml
-    sed "s,UPGRADE_FROM_VERSION,$releaseTag,g" scripts/upgrade_vars.template.yml > playbooks/group_vars/all/upgrade.yml
-    git commit -am "Reset upgrade variables after final release ${releaseTag}"
-fi
+# #reset upgrade playbook and variables if this is the final release
+# if [[ -z $LABEL_VERSION ]]; then
+#     echo "resetting upgrade playbook and variables after final release $releaseTag"
+#     cp scripts/upgrade.template.yml playbooks/upgrade.yml
+#     sed "s,UPGRADE_FROM_VERSION,$releaseTag,g" scripts/upgrade_vars.template.yml > playbooks/group_vars/all/upgrade.yml
+#     git commit -am "Reset upgrade variables after final release ${releaseTag}"
+# fi
 
-#push branch
-git push ${REMOTE} ${baseBranch}
-git push ${REMOTE} ${releaseTag}
+# #push branch
+# git push ${REMOTE} ${baseBranch}
+# git push ${REMOTE} ${releaseTag}
 
 #reset master upgrade playbook
 if [[ -z $LABEL_VERSION ]]; then
